f1f581bcca3d7f3f468fe3d52ca98056fcbe1a13
KubraNurGursoyyy <55765380+KubraNurGursoyyy@users.noreply.github.com>
2019-09-25 12:42:08 +0300

    Add files via upload

---
+++ Toplama - Kopya/Deniz Hocanın  Dersi 1/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Deniz_Hocanın__Dersi_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBirinciSayi = new System.Windows.Forms.TextBox();
+            this.lblBirinciSayi = new System.Windows.Forms.Label();
+            this.lblİkinciSayi = new System.Windows.Forms.Label();
+            this.txtİkinciSayi = new System.Windows.Forms.TextBox();
+            this.btnTopla = new System.Windows.Forms.Button();
+            this.FormTopla = new System.Windows.Forms.TabControl();
+            this.tabTopla = new System.Windows.Forms.TabPage();
+            this.tabLog = new System.Windows.Forms.TabPage();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtSonuc = new System.Windows.Forms.TextBox();
+            this.ListView1 = new System.Windows.Forms.ListView();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnSıfırla = new System.Windows.Forms.Button();
+            this.FormTopla.SuspendLayout();
+            this.tabTopla.SuspendLayout();
+            this.tabLog.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBirinciSayi
+            // 
+            this.txtBirinciSayi.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBirinciSayi.Location = new System.Drawing.Point(295, 45);
+            this.txtBirinciSayi.Name = "txtBirinciSayi";
+            this.txtBirinciSayi.Size = new System.Drawing.Size(154, 20);
+            this.txtBirinciSayi.TabIndex = 0;
+            this.txtBirinciSayi.TextChanged += new System.EventHandler(this.txtBirinciSayi_TextChanged);
+            this.txtBirinciSayi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtBirinciSayi_KeyPress);
+            // 
+            // lblBirinciSayi
+            // 
+            this.lblBirinciSayi.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblBirinciSayi.AutoSize = true;
+            this.lblBirinciSayi.BackColor = System.Drawing.Color.Transparent;
+            this.lblBirinciSayi.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblBirinciSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBirinciSayi.Location = new System.Drawing.Point(73, 45);
+            this.lblBirinciSayi.Name = "lblBirinciSayi";
+            this.lblBirinciSayi.Size = new System.Drawing.Size(131, 25);
+            this.lblBirinciSayi.TabIndex = 1;
+            this.lblBirinciSayi.Text = "Birinci Sayı";
+            // 
+            // lblİkinciSayi
+            // 
+            this.lblİkinciSayi.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblİkinciSayi.AutoSize = true;
+            this.lblİkinciSayi.BackColor = System.Drawing.Color.Transparent;
+            this.lblİkinciSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblİkinciSayi.Location = new System.Drawing.Point(73, 106);
+            this.lblİkinciSayi.Name = "lblİkinciSayi";
+            this.lblİkinciSayi.Size = new System.Drawing.Size(120, 25);
+            this.lblİkinciSayi.TabIndex = 2;
+            this.lblİkinciSayi.Text = "İkinci Sayı";
+            // 
+            // txtİkinciSayi
+            // 
+            this.txtİkinciSayi.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtİkinciSayi.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtİkinciSayi.Location = new System.Drawing.Point(295, 106);
+            this.txtİkinciSayi.Name = "txtİkinciSayi";
+            this.txtİkinciSayi.Size = new System.Drawing.Size(154, 20);
+            this.txtİkinciSayi.TabIndex = 1;
+            this.txtİkinciSayi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtİkinciSayi_KeyPress);
+            // 
+            // btnTopla
+            // 
+            this.btnTopla.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTopla.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnTopla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTopla.Location = new System.Drawing.Point(295, 209);
+            this.btnTopla.Name = "btnTopla";
+            this.btnTopla.Size = new System.Drawing.Size(154, 51);
+            this.btnTopla.TabIndex = 2;
+            this.btnTopla.Text = "Topla";
+            this.btnTopla.UseVisualStyleBackColor = true;
+            this.btnTopla.Click += new System.EventHandler(this.btnTopla_Click);
+            // 
+            // FormTopla
+            // 
+            this.FormTopla.Controls.Add(this.tabTopla);
+            this.FormTopla.Controls.Add(this.tabLog);
+            this.FormTopla.Location = new System.Drawing.Point(12, 12);
+            this.FormTopla.Name = "FormTopla";
+            this.FormTopla.SelectedIndex = 0;
+            this.FormTopla.Size = new System.Drawing.Size(801, 489);
+            this.FormTopla.TabIndex = 3;
+            // 
+            // tabTopla
+            // 
+            this.tabTopla.BackColor = System.Drawing.Color.White;
+            this.tabTopla.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabTopla.Controls.Add(this.txtSonuc);
+            this.tabTopla.Controls.Add(this.lblSonuc);
+            this.tabTopla.Controls.Add(this.lblBirinciSayi);
+            this.tabTopla.Controls.Add(this.btnTopla);
+            this.tabTopla.Controls.Add(this.txtBirinciSayi);
+            this.tabTopla.Controls.Add(this.txtİkinciSayi);
+            this.tabTopla.Controls.Add(this.lblİkinciSayi);
+            this.tabTopla.Location = new System.Drawing.Point(4, 22);
+            this.tabTopla.Name = "tabTopla";
+            this.tabTopla.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTopla.Size = new System.Drawing.Size(793, 463);
+            this.tabTopla.TabIndex = 0;
+            this.tabTopla.Text = "Topla";
+            // 
+            // tabLog
+            // 
+            this.tabLog.BackColor = System.Drawing.Color.Wheat;
+            this.tabLog.Controls.Add(this.btnSıfırla);
+            this.tabLog.Controls.Add(this.btnTemizle);
+            this.tabLog.Controls.Add(this.ListView1);
+            this.tabLog.Location = new System.Drawing.Point(4, 22);
+            this.tabLog.Name = "tabLog";
+            this.tabLog.Padding = new System.Windows.Forms.Padding(3);
+            this.tabLog.Size = new System.Drawing.Size(793, 463);
+            this.tabLog.TabIndex = 1;
+            this.tabLog.Text = "Log";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(73, 317);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(78, 25);
+            this.lblSonuc.TabIndex = 3;
+            this.lblSonuc.Text = "Sonuç";
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSonuc.Enabled = false;
+            this.txtSonuc.Location = new System.Drawing.Point(295, 317);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(154, 20);
+            this.txtSonuc.TabIndex = 4;
+            this.txtSonuc.TextChanged += new System.EventHandler(this.txtSonuc_TextChanged);
+            // 
+            // ListView1
+            // 
+            this.ListView1.BackColor = System.Drawing.Color.White;
+            this.ListView1.Location = new System.Drawing.Point(-4, 0);
+            this.ListView1.Name = "ListView1";
+            this.ListView1.Size = new System.Drawing.Size(797, 463);
+            this.ListView1.TabIndex = 0;
+            this.ListView1.UseCompatibleStateImageBehavior = false;
+            this.ListView1.View = System.Windows.Forms.View.List;
+            this.ListView1.SelectedIndexChanged += new System.EventHandler(this.ListView1_SelectedIndexChanged);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(95, 330);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(112, 44);
+            this.btnTemizle.TabIndex = 1;
+            this.btnTemizle.Text = "Seçileni Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnSıfırla
+            // 
+            this.btnSıfırla.Location = new System.Drawing.Point(285, 330);
+            this.btnSıfırla.Name = "btnSıfırla";
+            this.btnSıfırla.Size = new System.Drawing.Size(112, 44);
+            this.btnSıfırla.TabIndex = 2;
+            this.btnSıfırla.Text = " Hepsini Temizle";
+            this.btnSıfırla.UseVisualStyleBackColor = true;
+            this.btnSıfırla.Click += new System.EventHandler(this.btnSıfırla_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnTopla;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(840, 520);
+            this.Controls.Add(this.FormTopla);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormTopla";
+            this.Load += new System.EventHandler(this.FormTopla_Load);
+            this.Shown += new System.EventHandler(this.FormTopla_Shown);
+            this.FormTopla.ResumeLayout(false);
+            this.tabTopla.ResumeLayout(false);
+            this.tabTopla.PerformLayout();
+            this.tabLog.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBirinciSayi;
+        private System.Windows.Forms.Label lblBirinciSayi;
+        private System.Windows.Forms.Label lblİkinciSayi;
+        private System.Windows.Forms.TextBox txtİkinciSayi;
+        private System.Windows.Forms.Button btnTopla;
+        private System.Windows.Forms.TabControl FormTopla;
+        private System.Windows.Forms.TabPage tabTopla;
+        private System.Windows.Forms.TabPage tabLog;
+        private System.Windows.Forms.TextBox txtSonuc;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.ListView ListView1;
+        private System.Windows.Forms.Button btnSıfırla;
+        private System.Windows.Forms.Button btnTemizle;
+    }
+}
+
---
+++ Toplama - Kopya/Deniz Hocanın  Dersi 1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Deniz_Hocanın__Dersi_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        int Topla(int sayi1, int sayi2)
+        {
+            int toplam;
+            toplam = sayi1 + sayi2;
+            return toplam;
+        }
+
+
+        void EkranHazirla()
+        {
+            txtBirinciSayi.Text = "0";
+            txtİkinciSayi.Text = "0";
+            txtBirinciSayi.Focus();
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EkranHazirla();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            EkranHazirla();
+        }
+
+        private void txtBirinciSayi_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private Boolean Doğrula()
+        {
+            try
+            {
+                Convert.ToInt32(txtBirinciSayi.Text);
+                Convert.ToInt32(txtİkinciSayi.Text);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            if (Doğrula())
+            {
+                int birinciSayi = Convert.ToInt32(txtBirinciSayi.Text);
+                int ikinciSayi = Convert.ToInt32(txtİkinciSayi.Text);
+                txtSonuc.Text = Convert.ToString(Topla(birinciSayi, ikinciSayi));
+                LogYaz("İşlem Başarılı! Sonuç :" + Topla(birinciSayi, ikinciSayi));
+            }
+            else
+            {
+                MessageBox.Show("Girdiğiniz değerlerde hata var!");
+                LogYaz("İşlem Başarısız.");
+            }
+            EkranHazirla();
+        }
+
+        private void txtBirinciSayi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void txtİkinciSayi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void FormTopla_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormTopla_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSonuc_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void LogYaz(string mesaj)
+        {
+            ListView1.Items.Add(mesaj);
+
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            ListView1.Items.Remove(ListView1.FocusedItem);
+        }
+
+        private void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSıfırla_Click(object sender, EventArgs e)
+        {
+            ListView1.Items.Clear();
+        }
+    }
+}
